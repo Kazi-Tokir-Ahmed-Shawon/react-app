0601f8aa428b93c903454f841f82b6ac55dcb619
Kazi Tokir Ahmed Shawon <79966021+Kazi-Tokir-Ahmed-Shawon@users.noreply.github.com>
2023-07-15 00:00:39 +0600

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import React, { useState } from 'react';
-
+import PropTypes from 'prop-types';
 
 function Header({ title }) {
   return <h1>{title}</h1>;
 }
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+};
 
 function Content() {
   const [text, setText] = useState('');
@@ -29,13 +33,16 @@ function Content() {
 }
 
 function Footer() {
-  return <p>Created by [Your Name]</p>;
+  return <p>Created by Kazi Tokir Ahmed Shawon</p>;
 }
 
+Footer.propTypes = {
+  title: PropTypes.string.isRequired,
+};
 function App() {
   return (
     <div>
-      <Header title="My React App" />
+      <Header title="My Simple React App" />
       <Content />
       <Footer />
     </div>
